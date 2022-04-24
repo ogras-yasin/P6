9b6017da52d06143ffe46fa2bf21e08a9e37237b
Yasin Ogras <gs_yasin@hotmail.fr>
2022-04-24 13:50:23 +0200

    auth

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const Sauce = require('./models/modelsSauce')
+const Users = require('./models/users')
 
+const piquanteCtrl = require('./routes/piquante')
 
-
-app.use((req, res) => {
-    console.log(req.body)
-    console.log("req.body")
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-});
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://gs_yasin:azerty17@cluster0.fgdhg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true, 
@@ -17,10 +15,6 @@ mongoose.connect('mongodb+srv://gs_yasin:azerty17@cluster0.fgdhg.mongodb.net/myF
   .catch(() => console.log('Connexion à MongoDB échouée !'));
   
 
-module.exports = app;
-
-
- 
 
 
 app.use((req, res, next) => {
@@ -29,4 +23,46 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+app.post('/api/auth/signup',piquanteCtrl)
+
+
+
+app.use((req, res) => {
+  console.log(req.body)
+  console.log("req.body")
+ res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
+// app.use()
+
+
+  module.exports = app;
+
+
+  // app.post('/api/auth/signup',(req, res, next) => {
+  //   console.log('try to signup');
+  //   delete req.body._id;
+  //   const users = new Users({
+  //     ...req.body
+  //   })
+  //   users.save()
+  //   .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+  //   .catch(error => res.status(400).json({ error }));
+  // })
+
+
+
+
+  
+// app.post('api/auth/login',(req, res, next) => {
+//   console.log('try to login');
+//   Users.findOne({_id: req.params.id})
+//   .then(thing => res.status(200).json(thing))
+//   .catch(error => res.status(404).json({error }))
+// })
+
+// app.use('/api/stuff', stuffRoutes);
+// app.use('/api/auth', userRoutes);
+
 
---
+++ backend/routes/user.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+const userCtrl = require('../controllers/user');
+
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
+
+module.exports = router;
+
+
+
+
+
+
+// alinti 
+// const express = require("express");
+// const router = express.Router()
+
+// const userCtrl = require("../controllers/user");
+// const checkPassword = require("../middleware/check-password")
+// const checkEmail = require("../middleware/check-email")
+
+// router.post("/signup", checkEmail, checkPassword, userCtrl.signup);
+// router.post("/login", userCtrl.login);
+
+// module.exports = router;
