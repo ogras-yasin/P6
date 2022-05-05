72ced61325a647b6b2a9665d4bae034653f584fe
ogras yasin <gs_yasin@homail.fr>
2022-05-05 15:00:15 +0200

    versionner mes commits

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
+    //split on extrait le token de Bearer.  Donc le Bearer devra se trouver ds login 
+    // mais je ne trouve pas ds login je le trouve seulement ds sauces.
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
     const userId = decodedToken.userId;
