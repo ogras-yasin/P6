402495f732ce42ab7e05107481312e10e3330524
Yasin Ogras <gs_yasin@hotmail.fr>
2022-05-18 09:41:49 +0200

    ajout des likes

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -3,30 +3,50 @@ const Sauce = require("../models/modelsSauce");
 
 exports.createLike = (req, res, next) => {
   console.log("Je suis dans le controleur like !");
-  // res.status(200).res.Json({ msg: "like recu" });
-  // console.log("---> req");
-  // console.log(req);
 
-  // mise au format de l'id pour pouvoir aller chercher l'objet correspondant dans mongoDB
   console.log("---> req.params");
-  console.log(req.params.id);
-  // console.log("---> req.body");
-  // console.log(req.body);
+  console.log(req.params);
+  console.log("---> req.body");
+  console.log(req.body);
+  console.log("------------> _id: req.params.id ");
   console.log({ _id: req.params.id });
   // canim property
   // console.log({canim : req.params.id})
 
   // aller chercher l'object dans la base de l'objet
-  Sauce.findOne({ _id: req.params.id }).then((object) => {
-    console.log(object);
+  Sauce.findOne({ _id: req.params.id })
+    .then((object) => {
+      console.log("------------> sauce");
 
-    // utilisation de la methode javascript includes()
-    // si le usersLiked est false FALSE ET si like ===1
-    if (object.usersLiked.includes(req.body.userId) && req.body.likes === 1) {
-      console.log("instructions execute");
-    } else console.log("error");
-    res
-      .status(200).json({object})
-      .catch((error) => res.status(404).json({ msg: error }));
-  });
+      // utilisation de l'operateur $inc (mongoDB)
+      // utilisation de l'operateur $push (mongoDB)
+      // utilisation de l'operateur $pull (mongoDB)
+      console.log("--------->object.usersLiked");
+      console.log(object.usersLiked);
+      // utilisation de la methode javascript includes()
+      // si le usersLiked est false FALSE ET si like ===1 [ajout de like]
+      // dans ta requete ne met pas de guillement a 1 ["like" : 1]
+      if (!object.usersLiked.includes(req.body.userId) && req.body.like === 1) {
+        console.log("instructions execute");
+        console.log(req.body);
+
+        Sauce.updateOne(
+          { _id: req.params.id },
+          { $inc: { likes: 1 } },
+          // LIKES WORK BUT PUSH dosnt work
+          // je n'arrive pas a pusher userdId dans le tableau
+          { $push: { usersliked: req.body.userId } }
+        )
+          .then(() =>
+            res.status(201).json({ message: "like +1 / userid push", object })
+          )
+          .catch((error) => res.status(404).json({ error }));
+
+        // res.status(201).json("ajoute un like avec succes");
+      } else {
+        console.log("___> instruction non execute");
+        res.status(201).json({ object });
+      }
+    })
+    .catch((error) => res.status(404).json({ msgaa: error }));
 };
